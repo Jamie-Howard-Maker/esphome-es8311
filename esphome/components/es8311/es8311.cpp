62ea45971485fc87ef2d72c2fa66dd0226d5ba54
Jamie-Howard-Maker <jamiehoward430@gmail.com>
2026-01-19 13:06:17 +0000

    Delete esphome/components directory

--- esphome/components/es8311/es8311.cpp
+++
@@ -1,8 +0,0 @@
-#include "es8311.h"
-
-namespace esphome {
-namespace es8311 {
-ES8311Init es8311_instance;
-// Add any function implementations if needed (setup is in header)
-}  // namespace es8311
-}  // namespace esphome
--- esphome/components/es8311_init/es8311.cpp
+++
@@ -1,8 +0,0 @@
-#include "es8311.h"
-
-namespace esphome {
-namespace es8311 {
-
-// Add any function implementations if needed (setup is in header)
-}  // namespace es8311
-}  // namespace esphome
