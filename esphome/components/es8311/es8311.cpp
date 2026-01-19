05befa84aaacb0c76185634d8ac40694e61f252e
Jamie-Howard-Maker <jamiehoward430@gmail.com>
2026-01-19 12:32:07 +0000

    Initialize ES8311 instance in es8311.cpp

--- esphome/components/es8311/es8311.cpp
+++ esphome/components/es8311/es8311.cpp
@@ -2,7 +2,7 @@
 
 namespace esphome {
 namespace es8311 {
-
+ES8311Init es8311_instance;
 // Add any function implementations if needed (setup is in header)
 }  // namespace es8311
 }  // namespace esphome
